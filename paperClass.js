257bc8aa2b55d07a60dc74c9e06512d7a4b62384
TaruDhir2007 <73949805+TaruDhir2007@users.noreply.github.com>
2020-12-01 00:00:49 +0530

    Add files via upload

---
+++ paperClass.js
@@ -0,0 +1,21 @@
+class waste{
+    constructor(x, y, radius, options){
+        var options = {
+       'isStatic' : false,
+       'density' : 1.2,
+       'friction ': 1,
+       'restitution' : 0}
+       this.body = Bodies.circle(100, 640, 5, options)
+       World.add(world, this.body)
+       this.body = loadImage("paper.png")
+    }
+    display(){
+        var pos =this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        ellipseMode(CENTER);
+        fill(230, 230, 0);
+        ellipse(0, 0, this.radius);
+        pop();
+      }
+    }
